260893cbdeea2ceac7b7f8834629aa6d93e917af
E005440 <E005440@DT001192.cignitiglobal.com>
2021-02-24 10:57:11 +0530

    project created

---
+++ GitRepo/src/MyPackage/MySampleGit.java
@@ -0,0 +1,12 @@
+package MyPackage;
+
+public class MySampleGit {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("welcome to git");
+
+	}
+
+}
